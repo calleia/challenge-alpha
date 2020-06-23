04e707f37d49057b5cb2fcf0ed85f0abd4a05c26
Fellipe Calleia <fellipe@calleia.com.br>
2020-06-23 20:54:52 -0300

    Create SearchHotelWireframeProtocol

---
+++ Hotel Urbano Challenge/Hotel Urbano Challenge/SearchHotelWireframeProtocol.swift
@@ -0,0 +1,13 @@
+//
+//  SearchHotelWireframeProtocol.swift
+//  Hotel Urbano Challenge
+//
+//  Created by Fellipe Calleia on 23/06/20.
+//  Copyright © 2020 Fellipe Calleia. All rights reserved.
+//
+
+import UIKit
+
+protocol SearchHotelWireframeProtocol {
+    func present(in window: UIWindow)
+}
