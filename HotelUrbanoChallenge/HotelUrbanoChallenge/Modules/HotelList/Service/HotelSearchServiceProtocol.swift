93fb94fab9b73642ba5b1529412b9b476c84dbca
Fellipe Calleia <fellipe@calleia.com.br>
2020-06-25 20:48:53 -0300

    Create HotelSearchServiceProtocol

---
+++ HotelUrbanoChallenge/HotelUrbanoChallenge/Modules/HotelList/Service/HotelSearchServiceProtocol.swift
@@ -0,0 +1,11 @@
+//
+//  HotelSearchServiceProtocol.swift
+//  HotelUrbanoChallenge
+//
+//  Created by Fellipe Calleia on 25/06/20.
+//  Copyright © 2020 Fellipe Calleia. All rights reserved.
+//
+
+import Foundation
+
+protocol HotelSearchServiceProtocol {}
