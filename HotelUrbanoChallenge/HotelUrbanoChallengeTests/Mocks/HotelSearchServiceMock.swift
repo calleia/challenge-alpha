419b32ec658ac103aea68d0c9a52233f580fd8fa
Fellipe Calleia <fellipe@calleia.com.br>
2020-06-25 20:54:57 -0300

    Create HotelSearchServiceMock

---
+++ HotelUrbanoChallenge/HotelUrbanoChallengeTests/Mocks/HotelSearchServiceMock.swift
@@ -0,0 +1,13 @@
+//
+//  HotelSearchServiceMock.swift
+//  HotelUrbanoChallengeTests
+//
+//  Created by Fellipe Calleia on 25/06/20.
+//  Copyright © 2020 Fellipe Calleia. All rights reserved.
+//
+
+import Foundation
+
+@testable import HotelUrbanoChallenge
+
+final class HotelSearchServiceMock: HotelSearchServiceProtocol {}
