2212fbc7764833b1c9e0b934c4c0a5547e9991e8
Fellipe Calleia <fellipe@calleia.com.br>
2020-06-24 21:35:25 -0300

    Create SearchHotelsInteractorProtocol

---
+++ HotelUrbanoChallenge/HotelUrbanoChallenge/Modules/HotelList/Interactor/SearchHotelsInteractorProtocol.swift
@@ -0,0 +1,11 @@
+//
+//  SearchHotelsInteractorProtocol.swift
+//  HotelUrbanoChallenge
+//
+//  Created by Fellipe Calleia on 24/06/20.
+//  Copyright © 2020 Fellipe Calleia. All rights reserved.
+//
+
+import Foundation
+
+protocol SearchHotelsInteractorProtocol {}
