24fc35b8eb8952c6ebcddf0b446f649815e6c030
Fellipe Calleia <fellipe@calleia.com.br>
2020-06-24 22:30:28 -0300

    Create HotelListViewMock

---
+++ HotelUrbanoChallenge/HotelUrbanoChallengeTests/Mocks/HotelListViewMock.swift
@@ -0,0 +1,13 @@
+//
+//  HotelListViewMock.swift
+//  HotelUrbanoChallengeTests
+//
+//  Created by Fellipe Calleia on 24/06/20.
+//  Copyright © 2020 Fellipe Calleia. All rights reserved.
+//
+
+import Foundation
+
+@testable import HotelUrbanoChallenge
+
+final class HotelListViewMock: HotelListView {}
