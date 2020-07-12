a0f98ac37f6963da34a36183d1ea438fdf54258f
Fellipe Calleia <fellipe@calleia.com.br>
2020-07-12 16:08:42 -0300

    Create SuggestionsPresenterProtocol

---
+++ HotelUrbanoChallenge/HotelUrbanoChallenge/Modules/Suggestions/Presenter/SuggestionsPresenterProtocol.swift
@@ -0,0 +1,11 @@
+//
+//  SuggestionsPresenterProtocol.swift
+//  HotelUrbanoChallenge
+//
+//  Created by Fellipe Calleia on 12/07/20.
+//  Copyright © 2020 Fellipe Calleia. All rights reserved.
+//
+
+import Foundation
+
+protocol SuggestionsPresenterProtocol {}
