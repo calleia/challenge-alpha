84a05f55ce4ce4f47988f310a3a2c9139efdd2f4
Fellipe Calleia <fellipe@calleia.com.br>
2020-07-12 16:45:09 -0300

    Create SuggestionsServiceResponse

---
+++ HotelUrbanoChallenge/HotelUrbanoChallenge/Modules/Suggestions/Entity/SuggestionsServiceResponse.swift
@@ -0,0 +1,11 @@
+//
+//  SuggestionsServiceResponse.swift
+//  HotelUrbanoChallenge
+//
+//  Created by Fellipe Calleia on 12/07/20.
+//  Copyright © 2020 Fellipe Calleia. All rights reserved.
+//
+
+import Foundation
+
+struct SuggestionsServiceResponse {}
