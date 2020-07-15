4b641479d01f3240efb9c7106969bc21d0db3e20
Fellipe Calleia <fellipe@calleia.com.br>
2020-07-14 21:50:48 -0300

    Fix suggestions service call test for empty input

--- HotelUrbanoChallenge/HotelUrbanoChallengeTests/GetSuggestionsInteractorTests.swift
+++ HotelUrbanoChallenge/HotelUrbanoChallengeTests/GetSuggestionsInteractorTests.swift
@@ -174,11 +174,11 @@ final class GetSuggestionsInteractorTests: XCTestCase {
     func testSuggestionsForEmptyLocation() throws {
         let expectation = self.expectation(description: "Service request")
         
-        self.interactor.suggestions(for: "Location Value") { result in
+        self.interactor.suggestions(for: "") { result in
             switch result {
             case .success(let suggestions):
                 XCTAssertEqual(suggestions, [])
-                XCTAssertEqual(self.suggestionsServiceMock.callCount, 1)
+                XCTAssertEqual(self.suggestionsServiceMock.callCount, 0)
             case .failure(_):
                 XCTFail()
             }
