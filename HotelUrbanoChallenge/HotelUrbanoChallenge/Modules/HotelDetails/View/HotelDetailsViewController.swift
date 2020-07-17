f61b9bc2513a722d28587ba7a99ee5ad83d5f1f9
Fellipe Calleia <fellipe@calleia.com.br>
2020-07-16 23:16:12 -0300

    Create HotelDetailsPresenterProtocol

--- HotelUrbanoChallenge/HotelUrbanoChallenge/Modules/HotelDetails/View/HotelDetailsViewController.swift
+++ HotelUrbanoChallenge/HotelUrbanoChallenge/Modules/HotelDetails/View/HotelDetailsViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 final class HotelDetailsViewController: UIViewController {
     
+    var presenter: HotelDetailsPresenterProtocol? = nil
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
