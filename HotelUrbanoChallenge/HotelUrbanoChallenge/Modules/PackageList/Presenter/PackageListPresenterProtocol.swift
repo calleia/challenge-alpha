cc03aa5d05aba407696f27bec506e67d456076f3
Fellipe Calleia <fellipe@calleia.com.br>
2020-07-20 22:56:56 -0300

    Create PackageListPresenterProtocol

---
+++ HotelUrbanoChallenge/HotelUrbanoChallenge/Modules/PackageList/Presenter/PackageListPresenterProtocol.swift
@@ -0,0 +1,11 @@
+//
+//  PackageListPresenterProtocol.swift
+//  HotelUrbanoChallenge
+//
+//  Created by Fellipe Calleia on 20/07/20.
+//  Copyright © 2020 Fellipe Calleia. All rights reserved.
+//
+
+import Foundation
+
+protocol PackageListPresenterProtocol {}
