9780a1d13d425d1759b74fba2b52890b0b89a073
Fellipe Calleia <fellipe@calleia.com.br>
2020-07-21 00:49:49 -0300

    Create PackageListWireframe

---
+++ HotelUrbanoChallenge/HotelUrbanoChallenge/Modules/PackageList/Wireframe/PackageListWireframe.swift
@@ -0,0 +1,19 @@
+//
+//  PackageListWireframe.swift
+//  HotelUrbanoChallenge
+//
+//  Created by Fellipe Calleia on 21/07/20.
+//  Copyright © 2020 Fellipe Calleia. All rights reserved.
+//
+
+import Foundation
+
+final class PackageListWireframe: PackageListWireframeProtocol {
+    
+    weak var viewController: PackageListViewController? = nil
+    
+    func presentDetails(for package: Package) {
+        // TODO: present details view controller
+    }
+    
+}
