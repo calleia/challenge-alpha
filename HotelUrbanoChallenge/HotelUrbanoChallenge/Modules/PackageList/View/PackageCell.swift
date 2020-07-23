56ca980550a54be907b7310ca40bb788ca219518
Fellipe Calleia <fellipe@calleia.com.br>
2020-07-23 19:25:46 -0300

    Create PackageCell

---
+++ HotelUrbanoChallenge/HotelUrbanoChallenge/Modules/PackageList/View/PackageCell.swift
@@ -0,0 +1,20 @@
+//
+//  PackageCell.swift
+//  HotelUrbanoChallenge
+//
+//  Created by Fellipe Calleia on 23/07/20.
+//  Copyright © 2020 Fellipe Calleia. All rights reserved.
+//
+
+import UIKit
+
+class PackageCell: UICollectionViewCell {
+    
+    static let identifier = "PackageCellIdentifier"
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
