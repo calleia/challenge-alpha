75f3123d7fd34d71694d35477871b1afb6c830ff
Fellipe Calleia <fellipe@calleia.com.br>
2020-07-27 13:43:35 -0300

    Create PackageDetailsViewController

---
+++ HotelUrbanoChallenge/HotelUrbanoChallenge/Modules/PackageDetails/View/PackageDetailsViewController.swift
@@ -0,0 +1,21 @@
+//
+//  PackageDetailsViewController.swift
+//  HotelUrbanoChallenge
+//
+//  Created by Fellipe Calleia on 27/07/20.
+//  Copyright © 2020 Fellipe Calleia. All rights reserved.
+//
+
+import UIKit
+
+final class PackageDetailsViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+}
+
+extension PackageDetailsViewController: PackageDetailsView {}
