6a41f1e304a6abfcf6fd11cb20d3ebfaa5d4096e
Fellipe Calleia <fellipe@calleia.com.br>
2020-07-27 13:23:38 -0300

    Create PackageDetailsWireframeProtocol

---
+++ HotelUrbanoChallenge/HotelUrbanoChallenge/Modules/PackageDetails/Wireframe/PackageDetailsWireframeProtocol.swift
@@ -0,0 +1,11 @@
+//
+//  PackageDetailsWireframeProtocol.swift
+//  HotelUrbanoChallenge
+//
+//  Created by Fellipe Calleia on 27/07/20.
+//  Copyright © 2020 Fellipe Calleia. All rights reserved.
+//
+
+import Foundation
+
+protocol PackageDetailsWireframeProtocol {}
