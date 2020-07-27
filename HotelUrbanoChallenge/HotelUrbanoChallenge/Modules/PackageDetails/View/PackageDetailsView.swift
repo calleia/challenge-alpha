163848b85590b312177c6d05d9bcbbe152003995
Fellipe Calleia <fellipe@calleia.com.br>
2020-07-27 13:38:32 -0300

    Create PackageDetailsView

---
+++ HotelUrbanoChallenge/HotelUrbanoChallenge/Modules/PackageDetails/View/PackageDetailsView.swift
@@ -0,0 +1,11 @@
+//
+//  PackageDetailsView.swift
+//  HotelUrbanoChallenge
+//
+//  Created by Fellipe Calleia on 27/07/20.
+//  Copyright © 2020 Fellipe Calleia. All rights reserved.
+//
+
+import Foundation
+
+protocol PackageDetailsView {}
