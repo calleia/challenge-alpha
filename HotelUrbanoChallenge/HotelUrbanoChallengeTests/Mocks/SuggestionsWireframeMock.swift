fe708d1ed029d5551f50664467b7660b23a54e73
Fellipe Calleia <fellipe@calleia.com.br>
2020-08-12 23:39:41 -0300

    Create SuggestionsWireframeMock

---
+++ HotelUrbanoChallenge/HotelUrbanoChallengeTests/Mocks/SuggestionsWireframeMock.swift
@@ -0,0 +1,15 @@
+//
+//  SuggestionsWireframeMock.swift
+//  HotelUrbanoChallengeTests
+//
+//  Created by Fellipe Calleia on 12/08/20.
+//  Copyright © 2020 Fellipe Calleia. All rights reserved.
+//
+
+import UIKit
+
+@testable import HotelUrbanoChallenge
+
+final class SuggestionsWireframeMock: SuggestionsWireframeProtocol {
+    func presentSuggestions(for location: String, in navigationController: UINavigationController) {}
+}
